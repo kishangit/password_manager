aa5d547b78d0bb5f9895ea2dbfde45b6a144cefc
Kishan Dalwadi <dalwadikishan456@gmail.com>
2024-07-05 15:47:30 +0530

    Added permissions and role for sharing a password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_user_password
+    @current_user_password ||= current_user.user_passwords.find_by(password_id: @password)
+  end
+  helper_method :current_user_password
 end

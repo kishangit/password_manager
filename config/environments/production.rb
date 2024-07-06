4d09aebced6522cd29620d543e9bb555e5103392
Kishan Dalwadi <dalwadikishan456@gmail.com>
2024-07-06 10:57:20 +0530

    Added fly.io deployment changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'password-manager-old-bird-8700.fly.dev' }
 end
